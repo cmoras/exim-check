f9b87f09d7ea83cc449c85e49ca4697242987c37
cmoras <moraclen@gmail.com>
2017-06-24 13:22:55 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-#initializing the varibles
+
+#initializing the varibles begins
 sep="=====================================";
 qu=`exim -bpc`;
+malcron=`exim -bp| | grep (*) | grep -oP "(?<=\().*(?=\))" | sort | uniq -c | sort -nk1 | tail -n 10`;
+count=`cat /var/log/exim_mainlog*| grep -oP "(?<=cwd\ /home\d).*(?=args) | sort |uniq -c | sort -nk1`;
+#initializing the varibles ends
+
 
 echo "This is a exim-check test script";
 echo $sep;
@@ -10,8 +15,26 @@ echo "the count is $qu";
 echo $sep;
 echo "\n";
 echo $sep;
+echo $sep;
 echo "Checking for spamming occuring possibly due to malware/cron"
-exim -bp
+echo "the section gives the count of users using sendmail via scripts. 
+echo $malcron;
+echo $sep;
+echo $sep;
+echo "Cross refencing with the log files : exim_mainlog";
+echo "Along with the username that is cause of mass outbount email, it will also provide the cwd's"
+echo $count;
+echo $sep;
+echo $sep;
+
+
+
+
+
+
+
+
+
 
 
 
